97204e2ff8217ab70444a4087eed94e9dafd7f78
DA3NAN <adnane.aitmalek20@gmail.com>
2023-05-06 10:40:40 +0100

    good

--- philo.c
+++ philo.c
@@ -12,6 +12,26 @@
 
 #include "philo.h"
 
+long long    get_time()
+{
+    struct timeval tmp;
+    long long    curr_time;
+    
+    gettimeofday(&tmp, NULL);
+    curr_time = (tmp.tv_sec * 1000) + (tmp.tv_usec / 1000);
+    return(curr_time);
+}
+
+long long    get_period(long long start_time)
+{
+    struct timeval tmp;
+    long long    curr_time;
+    
+    gettimeofday(&tmp, NULL);
+    curr_time = (tmp.tv_sec * 1000) + (tmp.tv_usec / 1000);
+    return(curr_time - start_time);
+}
+
 void *philosopher(void* arg) {
     philosopher_info* info = (philosopher_info*)arg;
     int id = info->id;
@@ -21,57 +41,63 @@ void *philosopher(void* arg) {
     int time_to_sleep = info->time_to_sleep;
     int time_to_eat = info->time_to_eat;
 
-    time_t start_time;
-    time_t end_time;
+    long long very_start;
+    long long start_time;
+    long long end_time;
 
+    very_start = get_time();
     while (1) {
-        printf("Philosopher %d is thinking...\n", id);
+        printf("|%lld|, Philosopher %d is thinking...\n", get_period(very_start), id);
 
         pthread_mutex_lock(left_fork);
-        printf("Philosopher %d picked up the left fork.\n", id);
+        printf("|%lld|, Philosopher %d picked up the left fork.\n", get_period(very_start), id);
 
         pthread_mutex_lock(right_fork);
-        printf("Philosopher %d picked up the right fork.\n", id);
+        printf("|%lld|, Philosopher %d picked up the right fork.\n", get_period(very_start), id);
 
-        printf("Philosopher %d is eating...\n", id);
-        time(&start_time);
+        printf("|%lld|, Philosopher %d is eating...\n", get_period(very_start), id);
+        start_time = get_time();
 
         while (1) {
-            time(&end_time);
-            if (end_time - start_time >= time_to_die / 1000) {
-                printf("Elapsed : %ld, Philosopher %d has died!\n", (end_time - start_time) * 1000, id);
+            end_time = get_time();
+            if (end_time - start_time >= time_to_die) {
+                printf("|%lld|, Philosopher %d has died!\n", get_period(very_start), id);
                 exit(0);
             }
 
-            if (end_time - start_time >= time_to_eat / 1000) {
+            if (end_time - start_time >= time_to_eat) {
                 break;
             }
 
-            // Sleep for a short amount of time to reduce CPU usage
+            //Sleep for a short amount of time to reduce CPU usage
             // usleep(1000);
         }
 
         pthread_mutex_unlock(right_fork);
-        printf("Philosopher %d put down the right fork.\n", id);
+        printf("|%lld|, Philosopher %d put down the right fork.\n", get_period(very_start), id);
 
         pthread_mutex_unlock(left_fork);
-        printf("Philosopher %d put down the left fork.\n", id);
+        printf("|%lld|, Philosopher %d put down the left fork.\n", get_period(very_start), id);
 
-        printf("Philosopher %d is sleeping...\n", id);
-        usleep(time_to_sleep);
+        printf("|%lld|, Philosopher %d is sleeping...\n", get_period(very_start), id);
+        usleep(time_to_sleep * 1000);
     }
 }
 
 int main(int argc, char** argv) {
-    int num_philosophers = atoi(argv[1]);
-    pthread_t philosophers[num_philosophers];
-    pthread_mutex_t forks[num_philosophers];
-    philosopher_info info[num_philosophers];
+    int num_philosophers;
+    pthread_t *philosophers;
+    pthread_mutex_t *forks;
+    philosopher_info *info;
 
     if (argc != 5) {
         printf("Usage: ./philo <number_of_philosophers> <time_to_die> <time_to_eat> <time_to_sleep>\n");
         return 1;
     }
+    num_philosophers = atoi(argv[1]);
+    philosophers = malloc(num_philosophers * sizeof(pthread_t));
+    forks = malloc(num_philosophers * sizeof(pthread_mutex_t));
+    info = malloc(num_philosophers * sizeof(philosopher_info));
 
     // Initialize the mutexes for the forks
     for (int i = 0; i < num_philosophers; i++) {
@@ -84,8 +110,8 @@ int main(int argc, char** argv) {
         info[i].left_fork = &forks[i];
         info[i].right_fork = &forks[(i + 1) % num_philosophers];
         info[i].time_to_die = atoi(argv[2]);
-        info[i].time_to_sleep = atoi(argv[3]);
-        info[i].time_to_eat = atoi(argv[4]);
+        info[i].time_to_sleep = atoi(argv[4]);
+        info[i].time_to_eat = atoi(argv[3]);
 
         pthread_create(&philosophers[i], NULL, philosopher, &info[i]);
     }

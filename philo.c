52f573c2c3aa3d4631cdbe3346bdfe5e355f9b04
DA3NAN <adnane.aitmalek20@gmail.com>
2023-05-07 16:19:08 +0100

    good

--- philo.c
+++ philo.c
@@ -32,10 +32,18 @@ long long    get_period(long long start_time)
     return(curr_time - start_time);
 }
 
+void print_message(long long very_start, int id, const char* message, pthread_mutex_t* shared_mutex)
+{
+    pthread_mutex_lock(shared_mutex);
+    printf("|%lld|, Philosopher %d %s\n", get_period(very_start), id, message);
+    pthread_mutex_unlock(shared_mutex);
+}
+
 void *philosopher(void* arg) {
-    philosopher_info* info;
-    pthread_mutex_t* left_fork;
-    pthread_mutex_t* right_fork;
+    philosopher_info*   info;
+    pthread_mutex_t*    left_fork;
+    pthread_mutex_t*    right_fork;
+    pthread_mutex_t*    shared_mutex;
     int id;
     int time_to_die;
     int time_to_sleep;
@@ -48,22 +56,23 @@ void *philosopher(void* arg) {
 	id = info->id;
 	left_fork = info->left_fork;
 	right_fork = info->right_fork;
+    shared_mutex = info->shared_mutex;
 	time_to_die = info->time_to_die;
 	time_to_sleep = info->time_to_sleep;
 	time_to_eat = info->time_to_eat;
     very_start = get_time();
     while (1) {
-        printf("|%lld|, Philosopher %d is thinking...\n", get_period(very_start), id);
+        print_message(very_start, id, "is thinking...", shared_mutex);
         pthread_mutex_lock(left_fork);
-        printf("|%lld|, Philosopher %d picked up the left fork.\n", get_period(very_start), id);
+        print_message(very_start, id, "picked up the left fork.", shared_mutex);
         pthread_mutex_lock(right_fork);
-        printf("|%lld|, Philosopher %d picked up the right fork.\n", get_period(very_start), id);
-        printf("|%lld|, Philosopher %d is eating...\n", get_period(very_start), id);
+        print_message(very_start, id, "picked up the right fork.", shared_mutex);
+        print_message(very_start, id, "is eating...", shared_mutex);
         start_time = get_time();
         while (1) {
             end_time = get_time();
             if (end_time - start_time >= time_to_die) {
-                printf("|%lld|, Philosopher %d has died!\n", get_period(very_start), id);
+                print_message(very_start, id, "has died!", shared_mutex);
                 exit(0);
             }
             if (end_time - start_time >= time_to_eat) {
@@ -73,10 +82,10 @@ void *philosopher(void* arg) {
             // usleep(1000);
         }
         pthread_mutex_unlock(right_fork);
-        printf("|%lld|, Philosopher %d put down the right fork.\n", get_period(very_start), id);
+        print_message(very_start, id, "put down the right fork.", shared_mutex);
         pthread_mutex_unlock(left_fork);
-        printf("|%lld|, Philosopher %d put down the left fork.\n", get_period(very_start), id);
-        printf("|%lld|, Philosopher %d is sleeping...\n", get_period(very_start), id);
+        print_message(very_start, id, "put down the left fork.", shared_mutex);
+        print_message(very_start, id, "is sleeping...", shared_mutex);
         usleep(time_to_sleep * 1000);
     }
 }
@@ -85,6 +94,7 @@ int main(int argc, char** argv) {
     int num_philosophers;
     pthread_t *philosophers;
     pthread_mutex_t *forks;
+    pthread_mutex_t shared_print;
     philosopher_info *info;
 
     if (argc != 5) {
@@ -95,7 +105,8 @@ int main(int argc, char** argv) {
     philosophers = malloc(num_philosophers * sizeof(pthread_t));
     forks = malloc(num_philosophers * sizeof(pthread_mutex_t));
     info = malloc(num_philosophers * sizeof(philosopher_info));
-    // Initialize the mutexes for the forks
+    // Initialize the mutexes for the forks, shared_print
+    pthread_mutex_init(&shared_print, NULL);
     for (int i = 0; i < num_philosophers; i++) {
         pthread_mutex_init(&forks[i], NULL);
     }
@@ -107,7 +118,7 @@ int main(int argc, char** argv) {
         info[i].time_to_die = atoi(argv[2]);
         info[i].time_to_sleep = atoi(argv[4]);
         info[i].time_to_eat = atoi(argv[3]);
-
+        info[i].shared_mutex = &shared_print;
         pthread_create(&philosophers[i], NULL, philosopher, &info[i]);
     }
     // Wait for the philosophers to finish
--- philo.h
+++ philo.h
@@ -19,13 +19,11 @@
 # include <pthread.h>
 # include <sys/time.h>
 
-#define LEFT (philosopher_id + number_of_philosophers - 1) % number_of_philosophers
-#define RIGHT (philosopher_id + 1) % number_of_philosophers
-
 typedef struct {
     int id;
     pthread_mutex_t* left_fork;
     pthread_mutex_t* right_fork;
+    pthread_mutex_t* shared_mutex;
     int time_to_die;
     int time_to_sleep;
     int time_to_eat;
